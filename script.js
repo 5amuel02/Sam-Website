c14fec9419861ef91a044b5785cf8ff203136132
Samuel Manik <165402119+5amuel02@users.noreply.github.com>
2024-10-18 22:26:27 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const wa = document.querySelector('.link button:nth-child(1)');
+
+wa.addEventListener('click', function(){
+    let nilai = prompt("Masukkan Nomor Wa anda!!");
+    alert("nomor anda adalah " + nilai + " Untuk informasi berikutnya akan kami sampaikan segera dengan menghubungi nomor anda Terimakasih");
+})
